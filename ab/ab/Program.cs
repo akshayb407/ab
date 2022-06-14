0990e632b53fda261c34f6419d1cb97bcbdfca10
akshayb407 <akshayb407@gmail.com>
2022-06-14 23:32:39 +0530

    UC1 added

--- ab/ab/Program.cs
+++ ab/ab/Program.cs
@@ -3,10 +3,37 @@
 namespace ab
 {
     internal class Program
+{
+    static void Main(string[] args)
     {
-        static void Main(string[] args)
+        Program program = new Program();
+        program.GetemployeeAttendance();
+
+    }
+
+    public void GetemployeeAttendance()
+    {
+        Random random = new Random();
+        int value = random.Next(0, 2);
+        if (value == 0)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Employee is absent");
+            Console.WriteLine(value);
+        }
+        else
+        {
+            Console.WriteLine("Employee is present");
+            Console.WriteLine(value);
         }
     }
 }
+}
+
+
+
+
+
+
+
+
+
